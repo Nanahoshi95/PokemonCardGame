dd6402eb7412199013fa271b3ad8fa86567bf74d
nanahoshi95 <yosuke09058@gmail.com>
2023-11-19 20:29:23 +0900

    Initial Commit

---
+++ PokemonCardGame/PokemonCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCardGameApp.swift
+//  PokemonCardGame
+//
+//  Created by yamaji on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
